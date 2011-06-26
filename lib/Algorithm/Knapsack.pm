887eb0af2b8989bc3a484f1f73bf2a7115062853
Steven Jenkins <steven.jenkins@gmail.com>
2011-06-25 22:25:14 -0400

    Add lib/Algorithm/Knapsack.pm as it is a core component.

---
+++ lib/Algorithm/Knapsack.pm
@@ -0,0 +1,96 @@
+package Algorithm::Knapsack;
+
+use warnings;
+use strict;
+use Math::BigInt;
+
+=head1 NAME
+
+Algorithm::Knapsack - core library for Algorithm::Knapsack components
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Contains components required by all Algorithm::Knapsack modules.
+
+=head1 EXPORT
+
+Does not export anything.
+
+=head1 AUTHOR
+
+Steven Jenkins, C<< <steven.jenkins at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-algorithm::knapsack at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Algorithm::Knapsack>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Algorithm::Knapsack::Utils
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Algorithm::Knapsack>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Algorithm::Knapsack>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Algorithm::Knapsack>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Algorithm::Knapsack/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2011 Steven Jenkins.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this program; if not, write to the Free
+Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+02111-1307 USA.
+
+
+=cut
+
+1; # End of Algorithm::Knapsack::Utils
